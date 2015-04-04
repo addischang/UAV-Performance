8f9822383ee50a1faa32c1a41d3a0c943483e5e6
addischang1991 <addischang1991@livemail.tw>
2015-04-05 01:46:14 +0800

    Add atmosphere data read

--- main.m
+++ main.m
@@ -1,3 +1,31 @@
-% This is the main file.
+% MAIN.M
+%   First  edtion : 05-Apr-2015 01:37:56
+%   Lasted edtion : 05-Apr-2015 01:37:56
+%
+% DESCRIPTION
+%   This file is the main file for "UAV-Performance". This is a software 
+%   which provide performance parameters analysis for fixed-wing unmanned 
+%   aerial vehicle. But every thing are still in develop.
+%
+% COPPYRIGHT
+%   Copy right 2015 UAV Laboratory, Dept. of Aerospace Engineering, Tamkang
+%   University.
+%
+% AUTHOOR INFORMACTIONS
+%   Chang, Wei-Chieh 
+%    addischang1991@gmail.com
+%   Huang,Kuan-Lin 
+%     breakfastho@yahoo.com.tw
+%   Liu, Yu-Lin 
+%     lightning.539418@gmail.com
+
+% Clear the command window and data.
+clear;clc;
+
+% Input the desired height which you want.
+Height = 100;
+
+rho = uavparameter(100);
+
 disp('UAV');
 disp('Performance');
--- uavparameter.m
+++ uavparameter.m
@@ -1,1 +1,25 @@
-name
+function removal = uavparameter( InputHeight )
+% UAVPARAMETER.M
+%   First  edtion : 05-Apr-2015 01:37:56
+%   Lasted edtion : 05-Apr-2015 01:37:56
+%
+% DESCRIPTION
+%   This file is the main file for "UAV-Performance". This is a software 
+%   which provide performance parameters analysis for fixed-wing unmanned 
+%   aerial vehicle. But every thing are still in develop.
+%
+% COPPYRIGHT
+%   Copy right 2015 UAV Laboratory, Dept. of Aerospace Engineering, Tamkang
+%   University.
+%
+% AUTHOOR INFORMACTIONS
+%   Chang, Wei-Chieh 
+%    addischang1991@gmail.com
+%   Huang,Kuan-Lin 
+%     breakfastho@yahoo.com.tw
+%   Liu, Yu-Lin 
+%     lightning.539418@gmail.com
+
+AtmosphereData = stdatm( InputHeight );
+AirDensity = AtmosphereData( 1, 6 );
+removal = AirDensity
