4a3e0c3cb8c6624dbd40398838b6097e9009c48b
addischang1991 <addischang1991@livemail.tw>
2015-04-06 23:13:07 +0800

    Add symbol list

--- uavparameter.m
+++ uavparameter.m
@@ -1,18 +1,17 @@
-function removal = uavparameter( InputHeight )
-% UAVPARAMETER.M
-%   First  edtion : 05-Apr-2015 01:37:56
-%   Lasted edtion : 05-Apr-2015 01:51:00
-%   Lasted modify : Chang, Wei-Chieh
+% uavparameter.m
+%   First  edition : 05-Apr-2015 01:37:56
+%   Lasted edition : 06-Apr-2015 23:09:10
 %
 % DESCRIPTION
-%   This is a file which store global variables. Those subfunction will 
-%   catch the global variables to compute parameters which we want.   
+%   This file is the main file for "UAV-Performance". This is a software 
+%   which provides performance parameters analysis for fixed-wing unmanned 
+%   aerial vehicle. But everything is still in developing stage.
 %
-% COPPYRIGHT
-%   Copy right 2015 UAV Laboratory, Dept. of Aerospace Engineering, Tamkang
+% COPYRIGHT
+%   Copyright 2015 UAV Laboratory, Dept. of Aerospace Engineering, Tamkang
 %   University.
 %
-% AUTHOOR INFORMACTIONS
+% AUTHOR INFORMATIONS
 %   Chang, Wei-Chieh 
 %    addischang1991@gmail.com
 %   Huang,Kuan-Lin 
@@ -20,12 +19,49 @@ function removal = uavparameter( InputHeight )
 %   Liu, Yu-Lin 
 %     lightning.539418@gmail.com
 
-global AirDensity Gravity Velocity 
-global Serf Weight CL
-Mass = 1.5;
+% Clear the command window and data.
+clear;clc;
 
-AtmosphereData = stdatm( InputHeight );
-AirDensity = AtmosphereData( :, 6 );
-Gravity = AtmosphereData( :, 2 );
-Weight = Mass .* Gravity
-removal = Weight;
+% Parameter explanation
+% angle of attack : alpha
+% free-stream velocity : V_inf
+% lift : L
+% drag : D
+% moment about the quarter chord : M_0.25c
+% moment about the leading edge : M_LE
+% density of the ambient air : rho_inf
+% reference area : S
+% moment : M
+% lift coefficient : CL
+% drag coefficient : CD
+% moment coefficient : CM
+% dynamic pressure : q_inf
+% Reynolds number : Re
+% chord length : c
+% Mach number : M_inf
+% speed of sound : a_inf
+% temperature : T_inf
+% zero-lift angle of attack : alpha_L0
+% lift slope : a_0
+% slope of the moment coefficient : m_0
+% ski_friction coefficient : CF
+% aerodynamic center : CM_ac
+% distance between aerodynamic center and quarter_chord : x_ac
+% aspect ratio : AR
+% taper ratio : lambda
+% Mach angle : mu
+% weight : W
+% thrust : T
+% induced drag : D_i
+% induced drag coefficient : CD_i
+% span efficiency factor : e
+% zero-lift parasite drag : D_0
+% zero-lift drag coefficient : CD_0
+% jet velocity : V_j
+% power available : P_A
+% propulsive efficiency : eta_p
+% power from engine shaft : P
+% number of propeller revolutions per second : N
+% advance ratio : J
+% pitch angle : beta
+% weight of fuel burned per unit thrust per unit time : c_t
