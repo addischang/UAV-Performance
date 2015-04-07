96ebdae5eb1b26ab92ef0248df598a17040d57b8
Huang, Kuan-Lin <breakfastho@yahoo.com.tw>
2015-04-08 01:01:20 +0800

    uavthrustaviliable.m

--- uavthrustaviliable.m
+++ uavthrustaviliable.m
@@ -1,1 +1,9 @@
 % uavthrustaviliable
+%propeller_efficiency = ?
+V_inf = linspace( 0, 25, 25 );
+P_A = T_A*V_inf
+T = D;
+J = V_inf./(N*D);
+T_A = propeller_efficiency*P/V_inf
+plot( V_inf,T_A,);
+%find min(abs(T_A-T))
