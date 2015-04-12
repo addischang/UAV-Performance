4ebedb582fc61228a9bb319866da1c6460692e1e
breakfastho <breakfastho@yahoo.com.tw>
2015-04-12 17:29:59 +0800

    uavclimb

--- uavclimb.m
+++ uavclimb.m
@@ -32,11 +32,11 @@ k =  1 / ( pi * e_fw * AR_fw );
 T_A = 29.225;
 
 % Rate of climb
-RC1 = (T_A/W(1,1));
-RC2 = (0.5.*rho_inf*(V_inf.^2).*( ( S_fw + S_bw ) / W(1,1 ) ) * CD_0 );
-RC3 = W(1,1)*2*k ./ ( (S_fw+ S_bw).*rho_inf*V_inf.^2 );
+RC1 = ( T_A / W( 1,1 ) );
+RC2 = ( 0.5 .* rho_inf * (V_inf .^2) .* ( ( S_fw + S_bw ) / W(1,1 ) ) * CD_0 );
+RC3 = W(1,1) * 2 * k ./ ( ( S_fw + S_bw ) .* rho_inf * V_inf.^2 );
 
-RC = V_inf .* (RC1-RC2(1,:)-RC3(1,:));
+RC = V_inf .* ( RC1 - RC2(1,:) - RC3(1,:));
 figure( CouF )
 CouF = CouF + 1;
 plot(V_inf, RC, V_inf, ones( size( RC ) ), '-.r');
@@ -51,7 +51,7 @@ max_climb_thi = asin((T_max/W) - (1/CL_CD_max));
 V_thi_max = ( 4* ( W(1,1)/ (S_fw +S_bw) *k)) /( rho_inf * eta_p * (P/W) )
 
 % max rate of climb
- [ a b ] = max(RC)
+ [ a b ] = max(RC);
  disp( [ ' Max rate of climb  ' num2str( a( 1, CouH ) ) ' while '...
           num2str( V_inf( 1, b ) ) '(m/s). ' ])
 
